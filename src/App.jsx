9ba029d702bdc3c2de0131a7176aaded3d3278a9
Ayush Rawat <ayushrawat3338@gmail.com>
2024-12-08 12:30:53 +0530

    adding Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import './App.css'
+import "./App.css";
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <>
-      <h1>Hello, World</h1>
+      <Home></Home>
     </>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/home.css";
+
+function Home() {
+  return (
+    <>
+      <section className="home">
+        <h1 id="name"> Ayush Rawat</h1>
+        <h2 id="about-me">Chasing Curiosity</h2>
+        {/* <i className="fa-brands fa-github" style={{ color: "#ffffff" }}></i> */}
+      </section>
+    </>
+  );
+}
+
+export default Home;
